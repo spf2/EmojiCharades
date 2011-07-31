9d5c438cf6f137b822e92df1b649e56176b5869c
Steve Farrell <steve@farrell.org>
2011-07-31 13:12:18 -0700

    first commit

---
+++ EmojiCharades/SetupViewController.h
@@ -0,0 +1,13 @@
+//
+//  SetupViewController.h
+//  EmojiCharades
+//
+//  Created by Steve Farrell on 7/31/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SetupViewController : UIViewController
+
+@end
---
+++ EmojiCharades/SetupViewController.m
@@ -0,0 +1,51 @@
+//
+//  SetupViewController.m
+//  EmojiCharades
+//
+//  Created by Steve Farrell on 7/31/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "SetupViewController.h"
+
+@implementation SetupViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
